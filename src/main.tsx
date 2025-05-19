b02878b95865947818557630c5af7e8b266c2243
Eduardo Ernesto Corrales Reyna <ecorrales1979@gmail.com>
2025-05-18 22:31:17 -0300

    feat: add react-router-dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './app.tsx';
 import './index.css';
+import { Router } from './ui/router/router.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Router />
   </StrictMode>
 );
