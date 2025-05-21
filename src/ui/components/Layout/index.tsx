dd552a089ea97c8d2b1ae94fd198cf07b84c19c7
Eduardo Ernesto Corrales Reyna <ecorrales1979@gmail.com>
2025-05-20 23:15:04 -0300

    style: create CSS variables for background color

--- src/ui/components/Layout/index.tsx
+++ src/ui/components/Layout/index.tsx
@@ -2,7 +2,7 @@ import { Outlet } from 'react-router-dom';
 
 export default function Layout() {
   return (
-    <div className="min-h-screen w-full bg-[#18181b]">
+    <div className="min-h-screen w-full bg-[var(--bg-light-color)] dark:bg-[var(--bg-dark-color)]">
       <Outlet />
     </div>
   );
